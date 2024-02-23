78b090376239495d54f52bedd9e344b1c0a996c6
Siddhaeth281441 <90822048+Siddhaeth281441@users.noreply.github.com>
2024-02-23 22:13:51 +0530

    RPF Dto  and RPF view Dto added

---
+++ VendorPortal.API/Models/Dto/RFPDto.cs
@@ -0,0 +1,14 @@
+﻿using VendorPortal.API.Models.Domain;
+
+namespace VendorPortal.API.Models.Dto
+{
+    public class RFPDto
+    {
+        public string Title { get; set; }
+        public string UploadDocument { get; set; }
+        public string Project { get; set; }
+        public string EndDate { get; set; }
+        public string CategoryId { get; set; }
+
+    }
+}
---
+++ VendorPortal.API/Models/Dto/RFPViewDto.cs
@@ -0,0 +1,14 @@
+﻿using VendorPortal.API.Models.Domain;
+
+namespace VendorPortal.API.Models.Dto
+{
+    public class RFPViewDto
+    {
+        public string Title { get; set; }
+        public string UploadDocument { get; set; }
+        public string Project { get; set; }
+        public string EndDate { get; set; }
+        public string CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
