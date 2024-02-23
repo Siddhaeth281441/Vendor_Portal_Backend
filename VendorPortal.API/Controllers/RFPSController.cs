d0673d24029aeb9faf0d3d305dc6681bbd352473
Siddhaeth281441 <90822048+Siddhaeth281441@users.noreply.github.com>
2024-02-23 22:11:09 +0530

    RFP controller added

---
+++ VendorPortal.API/Controllers/RFPSController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VendorPortal.API.Data;
+
+namespace VendorPortal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RFPSController : ControllerBase
+    {
+        private readonly VendorPortalDbContext dbContext;
+
+        public RFPSController(VendorPortalDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+     
+
+    }
+}
